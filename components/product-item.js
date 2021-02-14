56f147669c8b65b00b37bddd2c4ab118e4344f9f
Ivan Robles <i1robles@ucsd.edu>
2021-02-13 23:50:55 -0800

    Fixed style formating

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,100 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+    let shadow = this.attachShadow({mode: 'open'});
+
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', 'wrapper');
+
+    const button = document.createElement('button');
+    button.textContent = "Add to Cart";  //read this from local storage
+
+    const img = document.createElement('img');
+    img.src = this.getAttribute('img');
+
+    const title = document.createElement('p');
+    title.setAttribute('class', 'title');
+
+    const price = document.createElement('p');
+    price.setAttribute('class', 'price');
+
+    const style = document.createElement('style');
+    style.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .wrapper {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .wrapper > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .wrapper > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .wrapper > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    `
+
+    shadow.appendChild(wrapper);
+    shadow.appendChild(style);
+    wrapper.appendChild(img);
+    wrapper.appendChild(title);
+    wrapper.appendChild(price);
+    wrapper.appendChild(button);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,79 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  if (!localStorage.getItem("storedList")) {      //if not in local, fetch and add to local
+    let fakeStoreRequest = new Request('https://fakestoreapi.com/products');
+    fetch(fakeStoreRequest)
+    .then(fsPromise => fsPromise.json())
+    .then(fsResponse => localStorage.setItem("storedList", JSON.stringify(fsResponse)));
+  }
+
 });
+  
+  let cartCount = document.getElementById("cart-count");
+  let grabbedString = localStorage.getItem("storedList");
+  let grabbedArray = JSON.parse(grabbedString);
+  let productList = document.getElementById('product-list');
+  
+  for (var key in grabbedArray) {   //iterate through json objects
+    
+    let currProduct = document.createElement('product-item');
+    
+    currProduct.shadowRoot.childNodes[0].childNodes[0].setAttribute('src', grabbedArray[key]['image']);
+    currProduct.shadowRoot.childNodes[0].childNodes[1].textContent = grabbedArray[key]['title'];
+    currProduct.shadowRoot.childNodes[0].childNodes[2].textContent = grabbedArray[key]['price'];
+    currProduct.shadowRoot.childNodes[0].childNodes[3].setAttribute('button', grabbedArray[key]['button']);
+
+
+    currProduct.shadowRoot.childNodes[1].style = grabbedArray[key]['style'];
+
+    
+    productList.appendChild(currProduct);
+
+    
+    currProduct.shadowRoot.childNodes[0].childNodes[3].addEventListener('click', function() {
+      if (currProduct.shadowRoot.childNodes[0].childNodes[3].textContent == "Add to Cart") {   //if not added, add to cart
+        var currCount = cartCount.textContent;
+        currCount = +currCount + 1;
+        cartCount.textContent = currCount;
+        alert('Added to Cart!');
+
+        //local memory part//
+        let shopString = localStorage.getItem("shopCart");
+        let shopJson = JSON.parse(shopString);
+
+        //add this item to local
+        //shopJson['shopCart'].push(currProduct.shadowRoot.childNodes[0].childNodes[1].textContent);
+        localStorage.setItem("shopCart", JSON.stringify(shopJson));
+        currProduct.shadowRoot.childNodes[0].childNodes[3].textContent = "Remove from Cart";
+      }
+  
+      else if (currProduct.shadowRoot.childNodes[0].childNodes[3].textContent == "Remove from Cart") { //if already added, remove
+        var currCount = cartCount.textContent;
+        currCount = +currCount - 1;
+        cartCount.textContent = currCount;
+        alert('Removed from Cart.');
+
+        //local memory part//
+        let shopString = localStorage.getItem("shopCart");
+        let shopJson = JSON.parse(shopString);
+
+        //remove this item 
+        /*
+        for( var i = 0; i < shopJson.length; i++){ 
+          if ( shopJson[i] === currProduct.shadowRoot.childNodes[0].childNodes[1].textContent) { 
+              shopJson.splice(i, 1); 
+          }
+        }
+        */
+
+        localStorage.setItem("shopCart", JSON.stringify(shopJson));
+        currProduct.shadowRoot.childNodes[0].childNodes[3].textContent = "Add to Cart";
+      }
+      
+    }); 
+
+  }
+  
+
